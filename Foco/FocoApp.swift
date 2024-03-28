ffb3d16f7ec7a7897f5b959234d4c9736f7d0de2
Arya Adyatma <88531676+aryadytm@users.noreply.github.com>
2024-03-28 10:20:50 +0700

    Initial Commit

---
+++ Foco/FocoApp.swift
@@ -0,0 +1,32 @@
+//
+//  FocoApp.swift
+//  Foco
+//
+//  Created by Arya Adyatma on 28/03/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FocoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
